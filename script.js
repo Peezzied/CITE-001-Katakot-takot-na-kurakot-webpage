8045ffba7c44c7dfae3340234cc7e6abe7e8c037
Peezzied <gamerpeenoise@gmail.com>
2025-09-17 09:17:07 +0800

    Initialized Git

---
+++ script.js
@@ -0,0 +1,15 @@
+const colorChangeBoundSection = document.querySelectorAll("#colorChangeBound");
+
+function changeColor() {
+    const colors = ["#1a1816", "#282518", "#282c1d", "#212828"]
+    const selectedColor = colors[Math.floor(Math.random() * colors.length)];
+
+    console.log(selectedColor, colorChangeBoundSection);
+
+    colorChangeBoundSection.forEach(section => {
+        section.style.backgroundColor = selectedColor;
+    });
+}
+
+const button = document.getElementById("interactable");
+button.addEventListener("click", changeColor);
