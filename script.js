e9235f12e137f0162fad8e672cbf77b482952dd1
Peezzied <gamerpeenoise@gmail.com>
2025-09-23 22:00:12 +0800

    Added raining effect, added water effect, added references, fixed fonts, added content, added more effects

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const colorChangeBoundSection = document.querySelectorAll(".colorChangeBound");
 
 function changeColor() {
-    const colors = ["#1a1816", "#282518", "#282c1d", "#212828"]
-    const selectedColor = colors[Math.floor(Math.random() * colors.length)];
+  const colors = ["#1a1816", "#282518", "#282c1d", "#212828"]
+  const selectedColor = colors[Math.floor(Math.random() * colors.length)];
 
-    console.log(selectedColor, colorChangeBoundSection);
+  console.log(selectedColor, colorChangeBoundSection);
 
-    colorChangeBoundSection.forEach(section => {
-        section.style.backgroundColor = selectedColor;
-    });
+  colorChangeBoundSection.forEach(section => {
+    section.style.backgroundColor = selectedColor;
+  });
 }
 
 const button = document.getElementById("interactable");
 button.addEventListener("click", changeColor);
 
-changeColor();
 
+const aside = document.getElementById("floatingAside");
 
-  const aside = document.getElementById("floatingAside");
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY + window.innerHeight;
+  const pageHeight = document.documentElement.scrollHeight;
 
-  window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY + window.innerHeight;
-    const pageHeight = document.documentElement.scrollHeight;
-
-    if (scrollPosition >= pageHeight) {
-      // at bottom
-      aside.classList.add("hidden");
-    } else {
-      aside.classList.remove("hidden");
-    }
-  });
+  if (scrollPosition >= pageHeight) {
+    // at bottom
+    aside.classList.add("hidden");
+  } else {
+    aside.classList.remove("hidden");
+  }
+});
